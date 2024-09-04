ad5979c169eb9a424a1750c2058338a79cfb32b2
hongming21 <zxe1711@outlook.com>
2024-09-04 14:54:29 +0800

    new

--- inference.py
+++ inference.py
@@ -92,23 +92,25 @@ def seed_everything(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
+
 def main():
-    
     args = parse_args()
     num_steps, guidance, batch_size = args.num_steps, args.guidance, args.batch_size
     checkpoint = args.checkpoint
     logdir = args.logdir
-    seed=args.seed
+    seed = args.seed
     seed_everything(seed)
     bs_param = (args.s1, args.s2, args.b1, args.b2)
     schedule_mode = args.adjust_mode
-    model_name=checkpoint.split('/')[-1]
+    model_name = checkpoint.split('/')[-1]
+
     # Load Models #
     tango = Tango(checkpoint, args.device)
     vae, stft, model = tango.vae, tango.stft, tango.model
     scheduler = DDPMScheduler.from_pretrained("stabilityai/stable-diffusion-2-1", subfolder="scheduler")
     text_prompts = []
     filenames = []
+
     # Load Data #
     for line in open(args.test_file).readlines():
         data = json.loads(line)
@@ -122,24 +124,30 @@ def main():
     output_dir = f"{logdir}/{exp_id}_steps_{num_steps}_guidance_{guidance}_s1_{args.s1}_s2_{args.s2}_b1_{args.b1}_b2_{args.b2}_{schedule_mode}_{model_name}_seed{seed}"
     os.makedirs(output_dir, exist_ok=True)
     
-    # Generate #
-    all_outputs = []
+    # Generate and Save #
     model.set_bs(schedule_mode, bs_param)
     print(model.sch_mode)
+    
     for k in tqdm(range(0, len(text_prompts), batch_size)):
-        text = text_prompts[k: k + batch_size]
+        batch_text = text_prompts[k: k + batch_size]
+        batch_filenames = [f"{output_dir}/{filenames[i]}.wav" for i in range(k, min(k + batch_size, len(filenames)))]
         
+        # Check if all files in the batch already exist
+        if all(os.path.exists(filename) for filename in batch_filenames):
+            print(f"All files in batch {k // batch_size + 1} already exist, skipping...")
+            continue
+        
+        # Generate the batch
         with torch.no_grad():
-            latents = model.inference(text, scheduler, num_steps, guidance)
+            latents = model.inference(batch_text, scheduler, num_steps, guidance)
             mel = vae.decode_first_stage(latents)
             wave = vae.decode_to_waveform(mel)
-            all_outputs += [item for item in wave]
+        
+        # Save all generated files in the batch
+        for i, wav in enumerate(wave):
+            sf.write(batch_filenames[i], wav, samplerate=16000)
     
-    # Save #
-    for i, wav in enumerate(all_outputs):
-        output_filename = f"{output_dir}/{filenames[i]}.wav"
-        sf.write(output_filename, wav, samplerate=16000)
-    
+    # Evaluation #
     evaluator = EvaluationHelper(16000, "cuda:0")
     
     result = evaluator.main(output_dir, args.test_references)
@@ -154,7 +162,6 @@ def main():
     result_filename = f"{logdir}/tango_checkpoint_summary.jsonl"
     with open(result_filename, "a") as f:
         f.write(json.dumps(result) + "\n\n")
-
 
 if __name__ == "__main__":
     main()
--- inference.sh
+++ inference.sh
@@ -1,21 +1,17 @@
 export TOKENIZERS_PARALLELISM=true 
-export CUDA_VISIBLE_DEVICES=3 
-
-for seed in 42 12 21
-do
-    python inference.py \
+export CUDA_VISIBLE_DEVICES=6 
+python inference.py \
     --checkpoint declare-lab/tango2 \
     --num_steps 200 \
     --guidance 3 \
     --batch_size 48 \
     --test_file /raid2/DATA/ckpt/audiocaps_test/test_subset.jsonl \
     --test_references /raid2/DATA/ckpt/audiocaps_test/test_sub \
-    --logdir ./log\
-    --adjust_mode sigmoid \
-    --seed $seed\
-    --s1 0.6 \
-    --s2 0.2 \
-    --b1 1.6 \
-    --b2 1.2 \
+    --logdir ./s-high\
+    --adjust_mode constant \
+    --seed 42\
+    --s1 2.0 \
+    --s2 2.0 \
+    --b1 1.0 \
+    --b2 1.0 \
 
-done
